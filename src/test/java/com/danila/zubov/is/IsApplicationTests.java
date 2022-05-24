b4eb0301d883e83303662888b59d13399e34d70a
Danila <danilazubov31@gmail.com>
2022-05-24 17:41:01 +0300

    init project

---
+++ src/test/java/com/danila/zubov/is/IsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danila.zubov.is;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
